a6f8dc48967408e76a2abea4791a465de8a309bd
Daniel Sun <sunlan@apache.org>
2021-06-14 15:48:25 +0800

    Tweak `ProxyMethodHandle`

--- src/main/java/org/codehaus/groovy/vmplugin/v16/ProxyMethodHandle.java
+++ src/main/java/org/codehaus/groovy/vmplugin/v16/ProxyMethodHandle.java
@@ -2,16 +2,23 @@ package org.codehaus.groovy.vmplugin.v16;
 
 import org.codehaus.groovy.GroovyBugError;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 class ProxyMethodHandle {
-    private static final Method INVOKE_DEFAULT_METHOD;
+    private static final MethodHandle INVOKE_DEFAULT_METHOD_HANDLE;
     static {
         try {
-            INVOKE_DEFAULT_METHOD = InvocationHandler.class.getDeclaredMethod("invokeDefault", Object.class, Method.class, Object[].class);
-        } catch (NoSuchMethodException e) {
+            // `invokeDefault` is JDK 16+ API, but we still build Groovy with JDK11,
+            // so use method handle instead of invoking the method directly
+            INVOKE_DEFAULT_METHOD_HANDLE = MethodHandles.lookup().findStatic(
+                                                InvocationHandler.class, "invokeDefault",
+                                                MethodType.methodType(Object.class, Object.class, Method.class, Object[].class));
+        } catch (NoSuchMethodException | IllegalAccessException e) {
             throw new GroovyBugError(e);
         }
     }
@@ -25,6 +32,7 @@ class ProxyMethodHandle {
     }
 
     Object invokeWithArguments(Object... arguments) throws Throwable {
-        return INVOKE_DEFAULT_METHOD.invoke(null, proxy, method, arguments);
+        Object proxy = this.proxy;
+        return INVOKE_DEFAULT_METHOD_HANDLE.invokeExact(proxy, method, arguments);
     }
 }
